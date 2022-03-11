bcde96c03b0f43a4f5cfc13b24f72ca1a264f9c8
Luca Tobón <96082778+Lucarte@users.noreply.github.com>
2022-03-11 22:27:24 +0100

    first version

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+CountriesList = [
+    {
+    name: 'Colombia',
+    capital: 'Bogota',
+    population: 80000000,
+    borders: ['BRA', 'ECU', 'PAN', 'PER', 'VEN']
+    },
+    {
+    name: 'USA',
+    capital: 'Washington D.C.',
+    population: 330000000,
+    borders: ['CAN', 'MEX']
+    },
+    {
+    name: 'Germany',
+    capital: 'Berlin',
+    population: 83000000,
+    borders: ['AUT', 'BEL', 'CZE', 'DNK', 'FRA', 'LUX', 'NLD', 'POL', 'CHE']
+    }
+ ];
